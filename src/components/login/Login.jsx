090f54a163f1e9ece75d1f9cf6a9e053228230c0
k4mimi <camilagmwz@gmail.com>
2025-10-24 17:33:54 -0500

    Login terminado y base inicial de tablero

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { useAuthContext } from "../../context/AuthContext";
+import { useNavigate, Link } from "react-router-dom";
+import "./Login.css";
+
+export const Login = () => {
+  const { login, user } = useAuthContext();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const success = await login(email, password);
+    if (success) {
+      // Redirigir según rol
+      if (user.rol === "Administrador") {
+        navigate("/principal");
+      } else {
+        navigate("/bienvenido");
+      }
+    } else {
+      alert("Credenciales inválidas");
+    }
+  };
+
+  return (
+    <div className="login-fondo">
+      <div className="login-tarjeta">
+        <h2 className="login-titulo">Inicio de Sesión</h2>
+        <form className="login-formulario" onSubmit={handleSubmit}>
+          <label className="login-label" htmlFor="email">Email:</label>
+          <input
+            className="login-input"
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+
+          <label className="login-label" htmlFor="password">Contraseña:</label>
+          <input
+            className="login-input"
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+
+          <button type="submit" className="login-boton">Iniciar sesión</button>
+        </form>
+        <p className="login-registro">
+          ¿No tienes cuenta? <Link to="/registro">Regístrate</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
